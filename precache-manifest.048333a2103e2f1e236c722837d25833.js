90b9b2b6f26b14648bc42021eb40d6d5aa131f07
Sandeep Bohra <sandeepbohra.sb@gmail.com>
2020-06-04 19:29:08 +0530

    Updates

---
+++ precache-manifest.048333a2103e2f1e236c722837d25833.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c76acc22c281d7f86cf1fcced285d8bc",
+    "url": "/demo-app/index.html"
+  },
+  {
+    "revision": "6395fa4094dcaf9a6b5a",
+    "url": "/demo-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "029f1973d121c4012cb5",
+    "url": "/demo-app/static/js/2.e9455646.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/demo-app/static/js/2.e9455646.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6395fa4094dcaf9a6b5a",
+    "url": "/demo-app/static/js/main.bb1f7ff6.chunk.js"
+  },
+  {
+    "revision": "937e10347c310d2130d0",
+    "url": "/demo-app/static/js/runtime-main.f193a713.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/demo-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
